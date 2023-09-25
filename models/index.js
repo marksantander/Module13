99bb57f72c1c1c7a461e98587d5a99e559326eea
marksantander <msntndr.ms@gmail.com>
2023-09-24 20:07:14 -0400

    added code to ./models/index.js

--- models/index.js
+++ models/index.js
@@ -12,6 +12,15 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+sync function getCategories() {
+  const categories = await Category.findAll();
+  return categories;
+}
+
+const categories = await getCategories();
+
+console.log(categories);
+
 module.exports = {
   Product,
   Category,
